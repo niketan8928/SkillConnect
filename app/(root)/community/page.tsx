3f014eca1638afcd18f6f1afae5364f0690e7924
nick8928 <nickg8928@gmail.com>
2024-11-15 20:04:15 +0530

    c2

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,18 +1,89 @@
+// import Filter from "@/components/shared/Filter";
+// import NoResult from "@/components/shared/NoResult";
+// import Pagination from "@/components/shared/Pagination";
+// import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+// import UserCard from "@/components/cards/UserCard";
+// import { getAllUsers, getUserById } from "@/lib/actions/user.action";
+// import { UserFilters } from "@/constants/filters";
+// import type { SearchParamsProps } from "@/types";
+// import type { Metadata } from "next";
+// import { auth } from "@clerk/nextjs";
+// import { Button } from "@/components/ui/button";
+// // import { useAuth } from "@clerk/nextjs";
+
+// export const metadata: Metadata = {
+//   title: "Community — BhimKatta",
+// };
+
+// const Page = async ({ searchParams }: SearchParamsProps) => {
+//   const result = await getAllUsers({
+//     searchQuery: searchParams.q,
+//     filter: searchParams.filter,
+//     page: searchParams.page ? +searchParams.page : 1,
+//   });
+//   const { userId: clerkId } = auth();
+//   const currUser = await getUserById({ userId: clerkId! });
+//   return (
+//     <>
+//       <h1 className="h1-bold text-dark100_light900">All Users</h1>
+
+//       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+//         <LocalSearchbar
+//           route="/community"
+//           iconPosition="left"
+//           imgSrc="/assets/icons/search.svg"
+//           placeholder="Search for amazing minds"
+//           otherClasses="flex-1"
+//         />
+
+//         <Filter
+//           filters={UserFilters}
+//           otherClasses="min-h-[56px] sm:min-w-[170px]"
+//         />
+//         <Button>Requests</Button>
+//       </div>
+
+//       <section className="mt-12 flex flex-wrap gap-4">
+//         {result.users.length > 0 ? (
+//           result.users.map((user: any) => (
+//             <UserCard key={user.clerkId} user={user} currentUser={currUser} />
+//           ))
+//         ) : (
+//           <NoResult
+//             title="No Users Found"
+//             description="Be the first to break the silence! 🚀 Signup to be the first and kickstart the community. Get involved! 💡"
+//             link="/sign-up"
+//             linkTitle="Sign Up"
+//           />
+//         )}
+//       </section>
+
+//       <div className="mt-10">
+//         <Pagination
+//           pageNumber={searchParams?.page ? +searchParams.page : 1}
+//           isNext={result.isNext}
+//         />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Page;
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
 import Pagination from "@/components/shared/Pagination";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import UserCard from "@/components/cards/UserCard";
-import { getAllUsers, getUserById } from "@/lib/actions/user.action";
+
+import { getAllUsers } from "@/lib/actions/user.action";
+
 import { UserFilters } from "@/constants/filters";
+
 import type { SearchParamsProps } from "@/types";
 import type { Metadata } from "next";
-import { auth } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-// import { useAuth } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: "Community — BhimKatta",
+  title: "Community — DevOverflow",
 };
 
 const Page = async ({ searchParams }: SearchParamsProps) => {
@@ -21,8 +92,7 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
     filter: searchParams.filter,
     page: searchParams.page ? +searchParams.page : 1,
   });
-  const { userId: clerkId } = auth();
-  const currUser = await getUserById({ userId: clerkId! });
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
@@ -40,13 +110,12 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
           filters={UserFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
-        <Button>Requests</Button>
       </div>
 
       <section className="mt-12 flex flex-wrap gap-4">
         {result.users.length > 0 ? (
           result.users.map((user: any) => (
-            <UserCard key={user.clerkId} user={user} currentUser={currUser} />
+            <UserCard key={user._id} user={user} />
           ))
         ) : (
           <NoResult
